d024431e730c48147037c5ccd188d4cef5014f66
hlb <hlb@handlino.com>
2010-01-19 03:09:38 +0800

    put data into issue page, and add some bare style

--- app/controllers/iphone/issues_controller.rb
+++ app/controllers/iphone/issues_controller.rb
@@ -4,12 +4,17 @@ class Iphone::IssuesController < ApplicationController
 
   before_filter :find_issue, :only => [:show]
   before_filter :find_project
+  helper :issues
+  helper :journals
 
   def index
     @issues = @project.issues.all(:conditions => [ "tracker_id = ?", params[:tracker_id] ])
   end
 
   def show
+    @journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
+    @journals.each_with_index {|j,i| j.indice = i+1}
+    @journals.reverse! if User.current.wants_comments_in_reverse_order?
   end
 
 private
